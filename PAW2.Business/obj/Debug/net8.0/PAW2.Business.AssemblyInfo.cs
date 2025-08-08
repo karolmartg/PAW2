496a5acd62e8248a8dc7a6f65f1f409fa589f787
karolmartg <karolmartgamb@hotmail.com>
2025-08-07 21:17:37 -0600

    update

--- PAW2.Business/obj/Debug/net8.0/PAW2.Business.AssemblyInfo.cs
+++ PAW2.Business/obj/Debug/net8.0/PAW2.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PAW2.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ccef0a09bda5e4a0bfa69e4e99c890fc297efd1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PAW2.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PAW2.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
